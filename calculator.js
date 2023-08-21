2c57330808433c6addef57c79d19d05a377b31ac
Splendid <ndubuisixtiangod@gmail.com>
2023-08-21 16:19:27 +0100

    another commit

--- calculator.js
+++ calculator.js
@@ -7,6 +7,9 @@ let calculator = document.getElementById("calculator");
 let buttons = document.querySelectorAll("button");
 let history = document.querySelector(".history");
 let calHis = document.getElementById("calHis");
+let facebookIcon = document.getElementById("facebook-icon");
+let twitterIcon = document.getElementById("twitter-icon");
+let instagramIcon = document.getElementById("instagram-icon");
 
 function display(value) {
   if (evaluate.innerText[0] === "0") {
@@ -62,7 +65,7 @@ function lightMode() {
   calSpace.style.background = "#ffffff";
   calSpace.style.color = "#273238";
   calButtons.style.background = "#f3f3f3";
-   history.style.background = "grey";
+   history.style.background = "#3c3c3c";
   for (const button of buttons) {
     button.style.color = "#3c3c3c";
   }
@@ -78,4 +81,54 @@ function closeHistory() {
 
 function clearHistory() {
   calHis.innerHTML = "";
+}
+
+
+
+function redirectToFacebook() {
+  let facebookLink = document.createElement("a");
+
+  facebookLink.href = "https://www.facebook.com/profile.php?id=100081341208797";
+
+  facebookLink.target = "_blank";
+
+  facebookLink.innerHTML =
+    '<i  class="bi bi-facebook" style="color: blue"></i>';
+
+  facebookIcon.parentNode.replaceChild(facebookLink, facebookIcon);
+
+  window.location.href =
+    "https://www.facebook.com/profile.php?id=100081341208797";
+}
+
+function Twitter() {
+  let twitterLink = document.createElement("a");
+
+  twitterLink.href =
+    "https://twitter.com/centsplendor3?t=UWqbHihMR0bHyiX80xNkDg&s=09";
+
+  twitterLink.target = "_blank";
+
+  twitterLink.innerHTML = '<i class="bi bi-twitter" style="color: blue"></i>';
+
+  twitterIcon.parentNode.replaceChild(twitterLink, twitterIcon);
+
+  window.location.href =
+    "https://twitter.com/centsplendor3?t=UWqbHihMR0bHyiX80xNkDg&s=09";
+}
+
+function Instagram() {
+  let instagramLink = document.createElement("a");
+  instagramLink.href =
+    "https://instagram.com/splendor_chris3?igshid=NTc4MTIwNjQ2YQ==";
+
+  instagramLink.target = "_blank";
+
+  instagramLink.innerHTML =
+    '<i class="bi bi-instagram" style="color: purple"></i>';
+
+  instagramIcon.parentNode.replaceChild(instagramLink, instagramIcon);
+
+  window.location.href =
+    "https://instagram.com/splendor_chris3?igshid=NTc4MTIwNjQ2YQ==";
 }
